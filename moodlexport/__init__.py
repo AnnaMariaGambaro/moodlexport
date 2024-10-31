e7a3dc4f838120ef3a4349df0f83b14c6045e533
AnnaMariaGambaro <104207858+AnnaMariaGambaro@users.noreply.github.com>
2024-10-31 16:33:17 +0100

    Update __init__.py

--- moodlexport/__init__.py
+++ moodlexport/__init__.py
@@ -2,9 +2,8 @@
 #from moodlexport.tex_to_python import *
 #from moodlexport.python_to_latex import *
 
-from .python_to_moodle import Category, Question, savexml, savetex, savepdf
+from .python_to_moodle import Category, Question #, printmk
 from .tex_to_python import latextopython, latextomoodle
-from .string_manager import printmk, includegraphics
 
 
 
